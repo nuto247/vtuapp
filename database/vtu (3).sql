dfca5caa3a36bf99ed88bef894498d1d498f7203
techbly <donchiwexco@gmail.com>
2024-03-25 15:33:21 +0100

    update

--- database/vtu (3).sql
+++ database/vtu (3).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 22, 2024 at 04:50 PM
+-- Generation Time: Mar 25, 2024 at 03:32 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -43,7 +43,9 @@ CREATE TABLE `deposits` (
 --
 
 INSERT INTO `deposits` (`id`, `user_id`, `status`, `reference`, `amount`, `transaction_id`, `created_at`, `updated_at`) VALUES
-(4, 8, 'completed', '1666816876', '200.00', 'MNFY|49|20240322162440|000066', '2024-03-22 14:24:36', '2024-03-22 14:25:09');
+(4, 8, 'completed', '1666816876', '200.00', 'MNFY|49|20240322162440|000066', '2024-03-22 14:24:36', '2024-03-22 14:25:09'),
+(9, 8, 'completed', '6445462420', '3000.00', 'MNFY|49|20240322174429|000072', '2024-03-22 15:44:27', '2024-03-22 15:44:48'),
+(10, 7, 'completed', '4353374561', '7000.00', 'MNFY|27|20240322175726|000091', '2024-03-22 15:57:23', '2024-03-22 15:57:53');
 
 -- --------------------------------------------------------
 
@@ -110,7 +112,49 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (8, '2018_11_15_124230_create_wallets_table', 2),
 (9, '2021_11_02_202021_update_wallets_uuid_table', 2),
 (10, '2023_07_29_000000_create_incoming_webhooks_table', 2),
-(11, '2024_03_19_065805_create_web_hook_calls_table', 2);
+(11, '2024_03_19_065805_create_web_hook_calls_table', 2),
+(12, '2024_03_23_134447_create_orders_table', 3),
+(13, '2024_03_23_135355_add_user_id_to_orders_table', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `order_reference` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `phone` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `amount` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `network` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `order_type` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `user_id`, `order_reference`, `phone`, `amount`, `network`, `order_type`, `status`, `created_at`, `updated_at`) VALUES
+(25, '8', NULL, '09034964142', '10', 'mtn', 'airtime', 'failed', '2024-03-25 12:20:38', '2024-03-25 12:20:40'),
+(26, '8', NULL, '09034964142', '10', 'mtn', 'airtime', 'failed', '2024-03-25 12:23:10', '2024-03-25 12:23:21'),
+(27, '8', NULL, '09034964142', '10', 'mtn', 'airtime', 'failed', '2024-03-25 12:25:10', '2024-03-25 12:25:12'),
+(28, '8', NULL, '09034964142', '10', 'mtn', 'airtime', 'failed', '2024-03-25 12:25:39', '2024-03-25 12:25:41'),
+(29, '8', NULL, '09034964142', '10', 'mtn', 'airtime', 'failed', '2024-03-25 12:31:48', '2024-03-25 12:31:58'),
+(30, '8', NULL, '09034964142', '10', 'mtn', 'airtime', 'failed', '2024-03-25 12:32:50', '2024-03-25 12:32:52'),
+(31, '8', NULL, '09034964142', '10', 'mtn', 'airtime', 'failed', '2024-03-25 12:36:43', '2024-03-25 12:36:53'),
+(32, '8', NULL, '09034964142', '10', 'mtn', 'airtime', 'pending', '2024-03-25 13:00:34', '2024-03-25 13:00:34'),
+(33, '8', NULL, '09034964142', '10', 'mtn', 'airtime', 'pending', '2024-03-25 13:06:50', '2024-03-25 13:06:50'),
+(34, '8', NULL, '09034964142', '10', 'mtn', 'airtime', 'failed', '2024-03-25 13:08:41', '2024-03-25 13:08:48'),
+(35, '8', '4394897', '09034964142', '10', 'mtn', 'airtime', 'success', '2024-03-25 13:15:17', '2024-03-25 13:15:27'),
+(36, '8', NULL, '09034964142', '10', 'mtn', 'airtime', 'pending', '2024-03-25 13:20:33', '2024-03-25 13:20:33'),
+(37, '8', NULL, '09034964142', '5', 'mtn', 'airtime', 'failed', '2024-03-25 13:24:48', '2024-03-25 13:25:20'),
+(38, '8', NULL, '09034964142', '10', 'mtn', 'airtime', 'pending', '2024-03-25 13:26:16', '2024-03-25 13:26:16'),
+(39, '8', '4394961', '09034964142', '10', 'mtn', 'airtime', 'success', '2024-03-25 13:29:31', '2024-03-25 13:29:40');
 
 -- --------------------------------------------------------
 
@@ -180,7 +224,7 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `usertype`, `name`, `email`, `wallet_address`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
 (7, 'admin', 'Bill Gates', 'admin@revolutpay.ng', '2403528767', NULL, '$2y$12$ydQTxj.ygKZcekUooAYHB.KIeWoOiqqUHy7FFHCgQD0zgmY8DJlB2', NULL, '2024-03-21 05:38:30', '2024-03-21 05:38:30'),
-(8, 'buyer', 'Onutochukwu Uche', 'uchetochukwuyu@gmail.com', '6866866797', NULL, '$2y$12$jfELNK4pAgTXlMoywVFV5.eJpSqP91Nc1Jvp0Abg7pqHfjdIe2sIa', 'y18u205lgwEd45q7gmRtiLhbhiCgQ1KIL1LC76DOHEXDDpwekU2s9pWucMrf', '2024-03-21 05:43:00', '2024-03-21 05:43:00');
+(8, 'buyer', 'Onutochukwu Uche', 'uchetochukwuyu@gmail.com', '6866866797', NULL, '$2y$12$jfELNK4pAgTXlMoywVFV5.eJpSqP91Nc1Jvp0Abg7pqHfjdIe2sIa', 'qctRngIAMdWCYHAaklzhN9RHjMxVBPxqn1mtdUAyeozdtY53aoM6r8IUhgiv', '2024-03-21 05:43:00', '2024-03-21 05:43:00');
 
 -- --------------------------------------------------------
 
@@ -210,7 +254,8 @@ CREATE TABLE `wallets` (
 INSERT INTO `wallets` (`id`, `holder_type`, `holder_id`, `name`, `slug`, `uuid`, `description`, `meta`, `balance`, `decimal_places`, `created_at`, `updated_at`) VALUES
 (1, 'App\\Models\\User', 4, 'Default Wallet', 'default', '267754e7-2fa6-4909-b2ac-6c123f151208', NULL, '[]', '420', 2, '2024-03-19 14:55:10', '2024-03-19 14:59:23'),
 (2, 'App\\Models\\User', 5, 'Default Wallet', 'default', '7a085ba5-812f-446e-b629-57861f47697b', NULL, '[]', '5000', 2, '2024-03-19 12:46:58', '2024-03-20 06:20:55'),
-(3, 'App\\Models\\User', 8, 'Default Wallet', 'default', 'ba9e4e1d-ac53-46d0-bf14-2a10b2dcf4e9', NULL, '[]', '5200', 2, '2024-03-21 06:03:08', '2024-03-22 14:25:23');
+(3, 'App\\Models\\User', 8, 'Default Wallet', 'default', 'ba9e4e1d-ac53-46d0-bf14-2a10b2dcf4e9', NULL, '[]', '8190', 2, '2024-03-21 06:03:08', '2024-03-25 13:29:42'),
+(4, 'App\\Models\\User', 7, 'Default Wallet', 'default', 'c55d81e0-69dc-4dc9-8eab-4257fdfe3785', NULL, '[]', '7000', 2, '2024-03-22 15:57:53', '2024-03-22 15:57:53');
 
 -- --------------------------------------------------------
 
@@ -243,7 +288,10 @@ INSERT INTO `wallet_transactions` (`id`, `payable_type`, `payable_id`, `wallet_i
 (4, 'App\\Models\\User', 5, 2, 'deposit', '2000', 1, NULL, 'e554658a-f59c-4cf9-903e-7253113d1da8', '2024-03-19 12:46:59', '2024-03-19 12:46:59'),
 (5, 'App\\Models\\User', 5, 2, 'deposit', '3000', 1, NULL, 'b2f24480-3a78-4f38-ac9c-f9aa2d53ea84', '2024-03-20 06:20:55', '2024-03-20 06:20:55'),
 (6, 'App\\Models\\User', 8, 3, 'deposit', '5000', 1, NULL, '4c82c973-e7e4-4384-ad12-a5f6edebfb9c', '2024-03-21 06:03:10', '2024-03-21 06:03:10'),
-(7, 'App\\Models\\User', 8, 3, 'deposit', '200', 1, NULL, 'ebfb1442-8ddd-40cd-802b-7904693718d8', '2024-03-22 14:25:22', '2024-03-22 14:25:22');
+(7, 'App\\Models\\User', 8, 3, 'deposit', '200', 1, NULL, 'ebfb1442-8ddd-40cd-802b-7904693718d8', '2024-03-22 14:25:22', '2024-03-22 14:25:22'),
+(8, 'App\\Models\\User', 8, 3, 'deposit', '3000', 1, NULL, 'f1e2b5cd-f20c-40b7-b702-5890a2955f9e', '2024-03-22 15:44:48', '2024-03-22 15:44:48'),
+(9, 'App\\Models\\User', 7, 4, 'deposit', '7000', 1, NULL, '6f27ac37-2e14-4a7e-bc3a-6910da6524dc', '2024-03-22 15:57:53', '2024-03-22 15:57:53'),
+(10, 'App\\Models\\User', 8, 3, 'withdraw', '-10', 1, NULL, '8d674553-dba1-4ee2-989d-4d77836b8602', '2024-03-25 13:29:41', '2024-03-25 13:29:41');
 
 -- --------------------------------------------------------
 
@@ -295,6 +343,12 @@ ALTER TABLE `incoming_webhooks`
 -- Indexes for table `migrations`
 --
 ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -367,7 +421,7 @@ ALTER TABLE `wallet_transfers`
 -- AUTO_INCREMENT for table `deposits`
 --
 ALTER TABLE `deposits`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -385,7 +439,13 @@ ALTER TABLE `incoming_webhooks`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
@@ -403,13 +463,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `wallets`
 --
 ALTER TABLE `wallets`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `wallet_transactions`
 --
 ALTER TABLE `wallet_transactions`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `wallet_transfers`
