06633f0c99bc2b02518a1bce894ced01195ec927
Onutochukwu Uche <uchetochukwu@gmail.com>
2024-06-04 13:24:58 +0100

    update data recharge
    update data recharge

--- resources/views/recharge-data1.blade.php
+++ resources/views/recharge-data1.blade.php
@@ -300,8 +300,7 @@
                 <div class="nk-footer">
                     <div class="container-fluid">
                         <div class="nk-footer-wrap">
-                            <div class="nk-footer-copyright"> &copy; 2022 DashLite. Template by <a
-                                    href="https://softnio.com" target="_blank">Softnio</a>
+                            <div class="nk-footer-copyright"> &copy; 2024 Revolutpay.ng</a>
                             </div>
                             <div class="nk-footer-links">
                                 <ul class="nav nav-sm">
